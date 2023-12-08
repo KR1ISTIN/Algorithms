dde405bed8eed6f8c980f155f8b5ea65c06ee75a
KR1ISTIN <kristin.desalme@gmail.com>
2023-12-07 23:20:38 -0600

    added solution for finding how many chars are in a string

--- new/exercises/maxchar/index.js
+++ new/exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// common questions
+// what is most common character in the string
+// does string A have same characters as string B (is it an anagram)
+// does the given string have repeaters characters
+
+// building a character map to loop throught the string 
+function maxChar(str) {
+    const charMap = {};
+  
+    // for each char in the str, loop over to see if it exist, 
+    // if exist then add 1, if does not then assign it to one
+    for(let char of str ) {
+        if(charMap[char]) {
+            charMap[char]++
+        } else {
+            charMap[char] = 1;
+        }
+    };
+    console.log(charMap);
+    console.log(charMap.l);
+
+};
+maxChar('Hello World')
 
 module.exports = maxChar;
