83f86fb7af9cb7569bd287f3ea2e983aa5515d3c
KR1ISTIN <kristin.desalme@gmail.com>
2023-12-07 23:33:57 -0600

    added function to find max letter and number of the letter

--- new/exercises/maxchar/index.js
+++ new/exercises/maxchar/index.js
@@ -13,9 +13,12 @@
 // building a character map to loop throught the string 
 function maxChar(str) {
     const charMap = {};
+    let max = 0; // max number value of the same character
+    let maxChar = ''; // setting to empty string so we can set the character of that highest number of uses
   
     // for each char in the str, loop over to see if it exist, 
     // if exist then add 1, if does not then assign it to one
+    // side note: a for of loop allows you to use it on any iterables like arrays, strings
     for(let char of str ) {
         if(charMap[char]) {
             charMap[char]++
@@ -24,7 +27,18 @@ function maxChar(str) {
         }
     };
     console.log(charMap);
-    console.log(charMap.l);
+    //console.log(charMap.l);
+
+    // for in allows you to loop through a key value pair, such as a object 
+    // char represents the key of the key value pairs in the object 
+    for(let char in charMap) {
+        // if number is greater than 0, assign new values 
+        if(charMap[char] > max) {
+            max = charMap[char];
+            maxChar = char;
+        }
+    }
+    return console.log(`max number of letter: ${max} / max letter: ${maxChar}`)
 
 };
 maxChar('Hello World')
