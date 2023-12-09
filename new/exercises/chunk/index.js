5136f3f7e56ed4a0e1ee905c8919286bb4a2c24e
KR1ISTIN <kristin.desalme@gmail.com>
2023-12-09 14:14:53 -0600

    added second solution to chunk file

--- new/exercises/chunk/index.js
+++ new/exercises/chunk/index.js
@@ -32,5 +32,35 @@ function chunk(array, size) {
 chunk([1, 2, 3, 4], 2);
 
 
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+
+// examples below: 
+// *** IMPORTANT*** start at index 0, 3 is the end index but does not include the value of index 3
+// slice will create a copy of the array but does not change the original array
+
+// const slice = letters.slice(0, 3);
+// console.log(slice);
+// console.log(letters);
+
+
+function chunkL(array, size) {
+    const chunked = [];
+    let index = 0;
+
+    while (index < array.length) {
+        // will create a sub array starting at index 0 and ending at index + size
+        // then index will be equal to index + size which would be 2
+        // the second loop will now start at index 2 and end at index 4
+
+        // slice will create multiple sub arrays within the chunked array
+        chunked.push(array.slice(index, index + size));
+        index+=size;
+        
+    }
+    console.log(chunked);
+};
+
+chunkL(letters, 2);
+
 
 module.exports = chunk;
