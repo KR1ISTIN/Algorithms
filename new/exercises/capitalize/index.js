40431fdc83b60279ad5cf7db874173298f96593f
KR1ISTIN <kristin.desalme@gmail.com>
2023-12-10 23:22:32 -0600

    solution to capitalize first letter

--- new/exercises/capitalize/index.js
+++ new/exercises/capitalize/index.js
@@ -7,6 +7,23 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    const words =[];
+
+    // split will create a new array splitting each word at the space
+    // loop through new array created by the split method 
+    for(let word of sliceStr.split(' ')) {
+        // take first letter from each vlaue and then join it with the rest of the word which we get from slice
+        // slice(1) will slice everything AFTER (not before) index 1 only
+        //console.log(word.slice(1)); 
+        //console.log(word[0].toUpperCase() + word.slice(1));
+
+        words.push((word[0].toUpperCase() + word.slice(1)));
+    }
+
+    console.log(words.join(' '));
+    return words.join(' ');
+}
+capitalize('a lazy fox'); 
 
 module.exports = capitalize;
