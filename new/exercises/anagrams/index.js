08023bb1bc021c5385e44418f5c4f809677cda33
KR1ISTIN <kristin.desalme@gmail.com>
2023-12-10 22:09:10 -0600

    anagram function

--- new/exercises/anagrams/index.js
+++ new/exercises/anagrams/index.js
@@ -4,13 +4,47 @@
 // in the same quantity. Only consider characters, not spaces
 // or punctuation.  Consider capital letters to be the same as lower case
 // --- Examples
-  anagrams('rail safety', 'fairy tales') --> True
-  anagrams('RAIL! SAFETY!', 'fairy tales') --> True
-  anagrams('Hi there', 'Bye there') --> False
+//   anagrams('rail safety', 'fairy tales') --> True
+//   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+//   anagrams('Hi there', 'Bye there') --> False
 
 function anagrams(stringA, stringB) {
+    const aCharMap = helperFunction(stringA);
+    const bCharMap = helperFunction(stringB);
 
-    
+    // IF DOES NOT HAVE SAME AMOUNT OF KEYS
+    if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+        console.log('not the same number of keys, therefore not an anagram');
+        return false
+    };
+
+    // IF DOES NOT HAVE SAME NUM OF CHARACTERS
+    for (let char in aCharMap) {
+        if (aCharMap[char] !== bCharMap[char]) {
+            console.log('does not have same number of characters')
+            return false;
+        }
+    }
+
+    console.log('is an anagram');
+    return true;
+
 }
 
+function helperFunction(str) {
+    const charMap = {};
+    
+    // any char that is not a number, capital char or lowercase character then replace it with nothing
+    for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+        charMap[char] = charMap[char] + 1 || 1;
+        console.log(charMap[char], char);
+    }
+
+    console.log(charMap);
+    return charMap;
+};
+
+  //anagrams('rail safety', 'fairy tales'); //true
+  anagrams('RAIL! SAFETY!', 'fairy taless'); //false
+  //anagrams('Hi there', 'Bye there'); // false
 module.exports = anagrams;
