4ab4bb9a46bf43d1c2f2fc05c90a2cb5166cb773
KR1ISTIN <kristin.desalme@gmail.com>
2023-12-11 23:18:31 -0600

    started matric solution

--- new/exercises/matrix/index.js
+++ new/exercises/matrix/index.js
@@ -15,6 +15,29 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    const results = [];
+
+    // n will result in creating x amount of subarrays
+    for (let i = 0; i < n; i++) {
+        results.push([])
+    }
+
+    // going to keep track of what number we are trying to push into our array results
+    let counter = 1;
+    let startColumn = 0;
+    let endColumn = n -1
+    let startRow = 0;
+    let endRow = n -1;
+
+    while (startColumn <= endColumn && startRow <= endRow) {
+        // top row
+        for (let i = startColumn; i <= endColumn; i++) {
+            results[startRow][i] = counter;
+            counter++
+        }
+        startRow++
+    }
+}   
 
 module.exports = matrix;
