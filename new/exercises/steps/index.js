d283ef80140e14403e40898d0aa9655ba445c954
KR1ISTIN <kristin.desalme@gmail.com>
2023-12-13 22:50:16 -0600

    added step solution 1

--- new/exercises/steps/index.js
+++ new/exercises/steps/index.js
@@ -17,6 +17,36 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// iterate 0 through n (make row)
+//make a empty str (stair)
+//from 0 to n (make column)
+// IF current column is = to or less than current row 
+// then add "#" to the stair
+//else
+// add space to the stair 
+
+
+function steps(n) {
+    for (let row = 0; row < n; row ++) {
+        // will create a empty string for every row that it iterated though to n
+        let stair = '.'; // empty string bc we need it to change 
+        console.log(stair, 'space')
+        // loop through n starting at 0 to make a column
+        // row processing loop 
+        for(let column = 0; column < n; column++){
+            if(column <= row) {
+                console.log( row, 'rrrr')
+                stair += "#"
+            } else {
+                // add a space  everytime 
+                console.log(column, 'col')
+                stair += ' ';            }
+        }
+
+        console.log(stair)
+    }
+}
+
+steps(5)
 
 module.exports = steps;
