6333d50f8f1bbc69398191d2804ab9cf09340510
KR1ISTIN <kristin.desalme@gmail.com>
2023-12-13 23:44:32 -0600

    added sol two for steps

--- new/exercises/steps/index.js
+++ new/exercises/steps/index.js
@@ -29,18 +29,18 @@
 function steps(n) {
     for (let row = 0; row < n; row ++) {
         // will create a empty string for every row that it iterated though to n
-        let stair = '.'; // empty string bc we need it to change 
-        console.log(stair, 'space')
+        let stair = ''; // empty string bc we need it to change 
+        console.log('space')
         // loop through n starting at 0 to make a column
         // row processing loop 
         for(let column = 0; column < n; column++){
             if(column <= row) {
-                console.log( row, 'rrrr')
+              
                 stair += "#"
             } else {
                 // add a space  everytime 
-                console.log(column, 'col')
-                stair += ' ';            }
+                stair += ' ';   
+            }
         }
 
         console.log(stair)
@@ -48,5 +48,35 @@ function steps(n) {
 }
 
 steps(5)
+console.log('-------------------solution TWO----------------')
+// solution two using recursion
+// A function is recursive if it calls itself and reaches a stop condition.
+// tips on recursion:
+    // find out bare minimum pieces of info to represent problem
+    // give a default to bare minimum
+    // check the base and if no work then return 
+    // or do some work and call function again but make sure to modify the arg 
+
+// setting row to default 0 so we have a starting position
+function stepsTwo (n, row=0, stair = '') {
+    // stopping point
+    if(n === row) {
+        return;
+    }
+   
+    if(n === stair.length) {
+        console.log(stair);
+        return stepsTwo(n, row +1);
+    }
+
+    if(stair.length <= row) {
+        stair += '#'
+    } else stair += ' ';
+    
+    stepsTwo(n, row, stair)
+
+}
+stepsTwo(3)
+
 
 module.exports = steps;
