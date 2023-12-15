7ac806dc5ac7dcf98cb2787c87fc29fd981f1c4c
KR1ISTIN <kristin.desalme@gmail.com>
2023-12-14 23:11:04 -0600

    updated comments for maxNum

--- new/exercises/maxNum/index.js
+++ new/exercises/maxNum/index.js
@@ -1,5 +1,16 @@
 // Write code to return the largest number in the given array
 
+// going to need to a variable to keep track of the max
+    // start the varible with the first positon of the array, 
+    // in the case below the inital state will be 1
+// going to need a loop
+// loop through the array
+// need to keep track of current value that is being passed
+// that value needs to compared to previous values that were assign to max 
+// if the current value of the index is greater than the previous assigned value, THEN reassign max
+// ** side note** max is going to get reassign multi times and always compare it to the next current value in the loop array 
+// dont forget to return 
+
 var maxNum = function(arr) {
     let max = arr[0]; // keeps track max number
 
@@ -9,12 +20,12 @@ var maxNum = function(arr) {
         if( current > max ) {
             max = current // reassigning max to whatever the current array index value is, now when it loops 
             //through again the new current index value will be compared to the the previous value that it was assign to 
-            console.log(current, 'current value' )
+            console.log(current, 'current value' );
         }
-        console.log(max, 'max temp')
+        console.log(max, 'max temp');
     }
-
-    console.log(`${max} is max number`)
+    console.log(`${max} is max number`);
+    return max;
 
 };
 maxNum([1,2,30,100]);
