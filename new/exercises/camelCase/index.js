a6fa3aa459ddcf3cc571e018ff08d36eb9666005
KR1ISTIN <kristin.desalme@gmail.com>
2023-12-27 19:54:24 -0600

    added second solution to camelCase file

--- new/exercises/camelCase/index.js
+++ new/exercises/camelCase/index.js
@@ -1,8 +1,8 @@
 // Write code to create a function that accepts a string and returns the string in camelCase
 
 
-
-var camelCase = function(str) {
+// camelCase for using an array 
+const camelCase = function(str) {
     const lowerCase = str.toLowerCase().split(' '); // turns into an array
     console.log(lowerCase, 'line 7')
     
@@ -31,3 +31,25 @@ var camelCase = function(str) {
 };
 
 camelCase('HElLo WoRlD nice To meet you!')
+
+// camelCase for using an str
+const camelCase2 = function(str) {
+    let result = "";
+    let words = str.toLowerCase().split(" "); // an array
+  
+    for (let i = 0; i < words.length; i++) { // loop through the array
+      let word = words[i]; // values from array
+      let letters = word.split(""); // now make another array to access letter from the word from the previous array
+      console.log(word, letters)
+  
+      if (i > 0) {
+        letters[0] = letters[0].toUpperCase(); // reassign the first position in the array to an uppercase
+      }
+  
+      result += letters.join(""); // now rejoin everything together
+    }
+  
+    return console.log(result);
+  };
+
+  camelCase2('over tHe RainBow we GO');
