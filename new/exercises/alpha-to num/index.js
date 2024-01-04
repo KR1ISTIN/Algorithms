d16fad550f8ff0bbec83bac982636f19d81746fe
KR1ISTIN <kristin.desalme@gmail.com>
2024-01-03 21:14:00 -0600

    added alpha-to-num folder and solution

---
+++ new/exercises/alpha-to num/index.js
@@ -0,0 +1,52 @@
+// given a string, replace every letter with its position in the alphabet.
+//If anything in the text isn't a letter, ignore it and don't return it.
+
+function alphabetPosition(text) {
+    const lc = text.toLowerCase(); // make it uniform
+    let str = ''; // will eq to str
+    const obj = { // give props their value
+      a:1,
+      b:2,
+      c:3,
+      d:4,
+      e:5,
+      f:6,
+      g:7,
+      h:8,
+      i:9,
+      j:10,
+      k:11,
+      l:12,
+      m:13,
+      n:14,
+      o:15,
+      p:16,
+      q:17,
+      r:18,
+      s:19,
+      t:20,
+      u:21,
+      v:22,
+      w:23,
+      x:24,
+      y:25,
+      z:26
+    }
+    
+    let result = lc.match(/[a-z]/g); // find only a-z within the str given
+    //console.log(result)
+    
+    for ( let char of result) {
+      console.log(char);
+       
+      const x = char;
+      
+      const num = obj[x];
+      //console.log(num);
+       str += num + ' ' // update the str value with a space between numbers
+    }
+    
+    return console.log(str)
+  }
+  
+  alphabetPosition("The sunset sets at twelve o' clock.")
--- new/exercises/arraySearch/index.js
+++ new/exercises/arraySearch/index.js
@@ -1,37 +1,53 @@
-// Write code to create a function that accepts a two-dimensional array
-// Each 2D element contains either the string "X" or "O" (both capitalized)
-// Return the number of times "X" appears in the 2D array
+// // Write code to create a function that accepts a two-dimensional array
+// // Each 2D element contains either the string "X" or "O" (both capitalized)
+// // Return the number of times "X" appears in the 2D array
 
-const arraySearch2D = function(arr) {
-    let countX = 0;
-    let countO = 0;
+// const arraySearch2D = function(arr) {
+//     let countX = 0;
+//     let countO = 0;
 
-    for(let i = 0; i < arr.length; i++) {
-        //console.log(arr[i]); // will log each value in the array
-        // now loop through each value's length and if x is less than the value
-        for(let x = 0; x < arr[i].length; x++ ) {
-            //console.log(x, arr[i][x]); // arr[i][x] is gaining access to the 2nd demsional array's value
+//     for(let i = 0; i < arr.length; i++) {
+//         //console.log(arr[i]); // will log each value in the array
+//         // now loop through each value's length and if x is less than the value
+//         for(let x = 0; x < arr[i].length; x++ ) {
+//             //console.log(x, arr[i][x]); // arr[i][x] is gaining access to the 2nd demsional array's value
             
-            // just wanted to try with 3d arr nest
-            for(let k = 0; k < arr[i][x].length; k++) {
-                console.log(k, arr[i][x])
-                if(arr[i][x] === 'X' || arr[i][x][k] === 'X') {
-                    countX++
-                } else if( arr[i][x] === 'O' ||arr[i][x][k] === 'O' ) {
-                    countO++
-                }
-            }
-        }
-    }
-    console.log(`CountX: ${countX}, CountO: ${countO}`)
+//             // just wanted to try with 3d arr nest
+//             for(let k = 0; k < arr[i][x].length; k++) {
+//                 console.log(k, arr[i][x])
+//                 if(arr[i][x] === 'X' || arr[i][x][k] === 'X') {
+//                     countX++
+//                 } else if( arr[i][x] === 'O' ||arr[i][x][k] === 'O' ) {
+//                     countO++
+//                 }
+//             }
+//         }
+//     }
+//     console.log(`CountX: ${countX}, CountO: ${countO}`)
 
-};
-arraySearch2D([1,2,3,['X','O'],4, ['X','X'],[['X','O']]]);
+// };
+// arraySearch2D([1,2,3,['X','O'],4, ['X','X'],[['X','O']]]);
 
-// make a function that takes and array and a cb function as parameters to return a new array
-function myFunction(numArr, cb) {
-    const newArr = numArr.map(num => cb(num));
-    console.log(newArr);
-    return newArr;
-};
-myFunction([1,2,3], num => num * 2);
+// // make a function that takes and array and a cb function as parameters to return a new array
+// function myFunction(numArr, cb) {
+//     const newArr = numArr.map(num => cb(num));
+//     console.log(newArr);
+//     return newArr;
+// };
+// myFunction([1,2,3], num => num * 2);
+
+function filter_list(l) {
+    // Return a new array with the strings filtered out
+    let newArr = []; 
+    
+    for ( let x of l) {
+      if(typeof  x === 'number') {
+        console.log( x)
+        newArr.push(x);
+      }
+    } 
+   
+    return console.log(newArr)
+  }
+  
+  filter_list([1,'a','b',0,15])
