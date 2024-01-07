397dd4433473c2b8bbca265c44d1ad5e930edaf7
KR1ISTIN <kristin.desalme@gmail.com>
2024-01-07 00:17:39 -0600

    added notes about regex and final solution for concert-flyers

--- new/exercises/concert-flyer/index.js
+++ new/exercises/concert-flyer/index.js
@@ -8,11 +8,13 @@ const concertFlyer = function (magazine, flyer) {
     // i can loop over the str and set the keys
     // if the key is not present set it to 1, else add one to the count for the value
 
+    // loop over second str and check if the map has the same key as the current value being evaluated in the second str
 
     const magazineMap = new Map();
+    const flyerMap = new Map();
 
-    for(let word of magazine.replace(/[^\w\s]/g, '').toLowerCase().split(' ')) {
-   
+    for(let word of magazine.replace(/[^\w\s']/g, '').toLowerCase().split(' ')) {
+        console.log(word)
         if(!magazineMap.has(word)) {
             magazineMap.set(word, 1)
         } else {
@@ -21,12 +23,30 @@ const concertFlyer = function (magazine, flyer) {
         }
     }
 
-    for(let word of flyer.replace(/[^\w\s]/g, '').toLowerCase().split(' ')) {
-     
+    for(let word of flyer.replace(/[^\w\s']/g, '').toLowerCase().split(' ')) {
+        if(!flyerMap.has(word)) {
+            flyerMap.set(word, 1)
+        } else {
+            const key = flyerMap.get(word);
+            flyerMap.set(word, key + 1)
+        }
+    
+    }
+
+    for(let [word] of flyerMap) {
+        //console.log(word);
+        if(flyerMap.get(word) !== magazineMap.get(word)) {
+            return false
+        }
     }
    
- 
+    return true
 };
 const str1 = 'Why are there so many songs about rainbows?';
 const str2 = 'so many songs';
-concertFlyer(str1,str2)
+concertFlyer(str1,str2);
+
+// /[^\w\s']/g: This regular expression defines a character class that matches any character that is not a word character 
+//(\w includes letters, digits, and underscores), not a whitespace character (\s), 
+//and not an apostrophe (').
+//.replace(): This method is used to replace the matched characters with an empty string, effectively removing them.
