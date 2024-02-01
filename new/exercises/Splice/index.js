e8fbf6aab6e2d68dd22f96a5448744c9b9e83000
KR1ISTIN <kristin.desalme@gmail.com>
2024-01-31 23:07:53 -0600

    added instructions for splice file

--- new/exercises/Splice/index.js
+++ new/exercises/Splice/index.js
@@ -14,3 +14,11 @@ function frankenSplice(arr1, arr2, n) {
   }
   
   frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+// You are given two arrays and an index.
+
+// Copy each element of the first array into the second array, in order.
+
+// Begin inserting elements at index n of the second array.
+
+// Return the resulting array. The input arrays should remain the same after the function runs.
